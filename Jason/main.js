e4fa25fc614aa59e968579a89400400da44d7c60
Luka Semenikhin <117368298+lukasemenikhin@users.noreply.github.com>
2023-03-27 18:55:07 +0400

    Add files via upload

---
+++ Jason/main.js
@@ -0,0 +1,14 @@
+const saiti = `{
+    "satauri":"What is Lorem Ipsum?",
+    "avtori":"Dean Winchester",
+    "categorie": ["sport", "bbc", "Sustainablity", "lead"],
+    "damateba":"27.03.2023",
+    "comments": 1981,
+    "image":"https://img.okezone.com/content/2017/03/31/406/1655474/pelabuhan-poto-tano-tempat-transit-terindah-di-sumbawa-tIFQFCagOk.JPG",
+    "agwera": "Lorem Ipsum is simply dummy text"
+    }`
+    
+    
+    const person = JSON.parse(saiti);
+    console.log(saiti)
+    
